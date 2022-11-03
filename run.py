0936746451bfee24b8ca63bcbc1be9be2eaec294
Saleh Sereshki <ssere004@ucr.edu>
2022-11-03 09:39:09 -0700

    First Commit

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-from tensorflow.python.ops.metrics_impl import recall
-
 import antibody_cnn as ac
 
 classifier_type = 'cnn'
@@ -7,10 +5,10 @@ antibody = 'CTLA-4'
 another_antibody = 'CTLA-4'
 
 for i in range(10):
-    accuracy, f1, precision, recalll = ac.classification(classifier_type, antibody, another_antibody, encode_method='onehot')
+    accuracy, f1, precision, encode_mat = ac.classification(classifier_type, antibody, another_antibody, encode_method='onehot')
     with open("classifier_acc.txt", "a") as file_object:
         file_object.write('%s\t%s\t%s\t%s\t%s\t%s\t%s' %(classifier_type, antibody, another_antibody, str(accuracy),
-                                                     str(f1), str(precision), str(recall)))
+                                                     str(f1), str(precision), str(encode_mat)))
         file_object.write("\n")
     print('%s\t%s\t%s\t%s\t%s\t%s\t%s' %(classifier_type, antibody, another_antibody, str(accuracy),
-                                                     str(f1), str(precision), str(recall)))
+                                                     str(f1), str(precision), str(encode_mat)))
